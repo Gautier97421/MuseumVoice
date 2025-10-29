af0faaa6cab838ebc742e768ccee2a08692c80b0
unknown <kaniobed28@gmail.com>
2025-10-29 17:17:12 +0100

    frontend-feat-03-created-lang-selector-component

--- frontend/museum-voice/src/App.js
+++ frontend/museum-voice/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './components/header/Header';
 import WelcomeMsg from './components/welcome_msg/WelcomeMsg';
-
+import LangSelector from './lang-selector/LangSelector';
 function App() {
   return (
     <>
     <Header />
     <WelcomeMsg />
+    <LangSelector />
     </>
   );
 }
---
+++ frontend/museum-voice/src/lang-selector/LangSelector.jsx
@@ -0,0 +1,13 @@
+// LangSelector.jsx
+import React from 'react';
+import './LangSelector.css';
+
+const LangSelector = () => (
+  <div className="lang-selector">
+    <span className="lang-option">FR</span>
+    <span className="lang-option active">EN</span>
+    <span className="lang-option">NL</span>
+  </div>
+);
+
+export default LangSelector;
