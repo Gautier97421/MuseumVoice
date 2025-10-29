00c9178f6c0a2ed27baf9cf91cd70abb12b79c41
unknown <kaniobed28@gmail.com>
2025-10-29 16:43:13 +0100

    frontend-feat-02-created-home-page-welcome-msg-component

--- frontend/museum-voice/src/App.js
+++ frontend/museum-voice/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header/Header';
+import WelcomeMsg from './components/welcome_msg/WelcomeMsg';
 
 function App() {
   return (
     <>
     <Header />
+    <WelcomeMsg />
     </>
   );
 }
